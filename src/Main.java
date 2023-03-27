217f1aa4112cdeb999fe22f8fef1b9244434925f
Ivan Sokolov <Sokolovpersonalmail@gmail.com>
2023-03-27 23:27:49 +0300

    Changed openProgress

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
+public class Main extends Operations {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (String directory : DIRECTORIES) {
+            makeDir(directory);
+        }
+        for (String file : FILES) {
+            makeFile(file);
+        }
+        generateLog(SB);
+        saveGame(new GameProgress(100,2,5,2.0), "C:/Games/savegames/savegame1.dat");
+        saveGame(new GameProgress(190,7,12,14.0), "C:/Games/savegames/savegame2.dat");
+        saveGame(new GameProgress(666,33,123,222.0), "C:/Games/savegames/savegame3.dat");
+        zipFiles("C:/Games/savegames/zip.zip", SAVESLIST);
+        deleteFiles(SAVESLIST);
+        openZip("C:/Games/savegames/zip.zip", "C:/Games/savegames");
+        openProgress(SAVESLIST);
     }
 }
--- src/Operations.java
+++ src/Operations.java
@@ -1,9 +1,11 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
-public class makeDirsAndFiles {
+public class Operations {
     public static StringBuilder SB = new StringBuilder();
     public static String[] DIRECTORIES = {"C:/Games/src/main", "C:/Games/src/test",
             "C:/Games/res/drawables", "C:/Games/res/vectors",
@@ -11,15 +13,15 @@ public class makeDirsAndFiles {
             "C:/Games/temp"};
     public static String[] FILES = {"C:/Games/src/main/Main.java",
             "C:/Games/src/main/Utils.java", "C:/Games/temp/temp.txt"};
+    public static List<String> SAVESLIST = new ArrayList<>();
 
-    public static File makeDir(String nameDir) {
+    public static void makeDir(String nameDir) {
         File dir = new File(nameDir);
         dir.mkdirs();
         SB.append("Каталог " + dir + " успешно создан.\n");
-        return dir;
     }
 
-    public static File makeFile(String nameFile) {
+    public static void makeFile(String nameFile) {
         File file = new File(nameFile);
         try {
             file.createNewFile();
@@ -27,7 +29,6 @@ public class makeDirsAndFiles {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return file;
     }
 
     public static void generateLog(StringBuilder sb) {
@@ -38,4 +39,72 @@ public class makeDirsAndFiles {
         }
     }
 
+    public static void saveGame(GameProgress gameProgress, String path) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(
+                new FileOutputStream(path))) {
+            oos.writeObject(gameProgress);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        SAVESLIST.add(path);
+    }
+
+    public static void zipFiles(String dirToSave, List<String> filesToZip) {
+        {
+            try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(dirToSave))) {
+                for (int i = 0; i < filesToZip.size(); i++) {
+                    FileInputStream fis = new FileInputStream(filesToZip.get(i));
+                    ZipEntry entry = new ZipEntry("savegame" + (i + 1) + ".dat");
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    fis.close();
+                    zout.closeEntry();
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void deleteFiles(List<String> filesToDelite) {
+        for (String save : filesToDelite) {
+            File file = new File(save);
+            if (file.delete()) {
+                System.out.println("Файл " + save + " удален успешно.");
+            }
+        }
+    }
+
+    public static void openZip(String zip, String dirToSave) {
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zip))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zis.getNextEntry()) != null) {
+                name = entry.getName();
+                try (FileOutputStream fos = new FileOutputStream(dirToSave + "/" + name)) {
+                    while (zis.read() != -1){
+                        fos.write(zis.read());
+                    }
+                    fos.flush();
+                    zis.closeEntry();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static GameProgress openProgress(List<String> filesToPrint) {
+        for (String file : filesToPrint) {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+                return (GameProgress) ois.readObject();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return null;
+    }
 }
